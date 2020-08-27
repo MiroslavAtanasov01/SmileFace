dbb6b77d407ff1de6b757e723bf658f0b5353b4d
Miroslav <fshn1994@abv.bg>
2020-08-27 23:58:12 +0300

    feat:implement logged out page and add ico

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import styles from './App.module.css'
+import Background from '../src/images/760933.jpg'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className={styles.main}>
+      <div>
+        <img src={Background}></img>
+      </div>
+      <div>
+        <h1>SmileFace</h1>
+        <div>
+          <form className={styles.form}>
+            <div>
+              <label htmlFor='username'>username</label>
+              <input type='text' value='username' />
+            </div>
+            <div>
+              <label htmlFor='username'>username</label>
+              <input type='text' value='username' />
+            </div>
+            <div>
+              <label htmlFor='username'>username</label>
+              <input type='text' value='username' />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
