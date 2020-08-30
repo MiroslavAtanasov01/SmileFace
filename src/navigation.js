1117e35648ee67bb6e1797122c9014bfcfe5901f
Miroslav <fshn1994@abv.bg>
2020-08-30 16:33:40 +0300

    feat:add context and navigation

---
+++ src/navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { useContext } from 'react'
+import UserContext from './Context'
+
+const Navigation = () => {
+    const context = useContext(UserContext)
+    const loggedIn = context.loggedIn
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/">
+                    {loggedIn ? (<Main />) : (<Redirect to="/login" />)}
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Navigation
