ec0c98709782e22bfb038b800cf54e66bad20155
MiroslavAtanasov <MiroslavAtanasow@gmail.com>
2021-05-12 15:16:31 +0300

    feat: restyle login page with material ui and remove console.errors

--- src/components/details/edit/index.js
+++ src/components/details/edit/index.js
@@ -29,7 +29,7 @@ const Edit = ({ closeMenu }) => {
                 })
                 closeMenu()
             } catch (err) {
-                console.error(err)
+                return err
             }
         }
     }
