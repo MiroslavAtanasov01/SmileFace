ddf64952893b338d95f1d3e5691c36377b647153
MiroslavAtanasov <MiroslavAtanasow@gmail.com>
2021-05-13 17:44:22 +0300

    feat: spicifying nodejs version

--- REST_API/config/routes.js
+++ REST_API/config/routes.js
@@ -1,7 +1,11 @@
 const router = require('../routes/')
+const path = require('path')
 
 module.exports = (app) => {
     app.use('/api/user', router.user)
     app.use('/api/post', router.post)
-    app.use('*', (req, res, next) => res.send('<h1> Error 404 Not Found </h1>'))
+    // app.use('*', (req, res, next) => res.send('<h1> Error 404 Not Found </h1>'))
+    app.use('*', (req, res, next) => {
+        res.sendFile(path.join(__dirname, '../../build', 'index.html'))
+    });
 }
